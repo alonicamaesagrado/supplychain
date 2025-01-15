5b66fe863e1c552fd5f9cd91df55b83f5a0e0187
itsalonica <alonicamaesagrado@gmail.com>
2025-01-15 20:04:37 +0800

    added put api for user

--- src/main/java/com/informatics/supplychain/controller/UserController.java
+++ src/main/java/com/informatics/supplychain/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class UserController {
     @GetMapping("v1/user")
     ResponseEntity<?> getUser(@RequestParam String usercode) {
         var user = userService.findByUserCodeAndStatus(usercode, StatusEnum.ACTIVE);
-        
+
         if (user == null) {
             return ResponseEntity.status(404).body("User not found.");
         }
@@ -61,13 +62,36 @@ public class UserController {
         return ResponseEntity.ok(new UserDto(userService.save(user)));
     }
 
+    @PutMapping("v1/user")
+    public ResponseEntity<?> updateUser(@RequestBody UserDto userDto) throws Exception {
+        String usercode = userDto.getUsercode();
+        var existingUser = userService.findByUserCodeAndStatus(userDto.getUsercode(), StatusEnum.ACTIVE);
+        if (existingUser == null) {
+            return ResponseEntity.status(404).body("User not found.");
+        }
+
+        var userGroup = userGroupService.findById(userDto.getUserGroup().getId());
+        if (userGroup == null) {
+            throw new Exception("User Group does not exist!");
+        }
+
+        existingUser.setPassword(userDto.getPassword());
+        existingUser.setFirst_name(userDto.getFirst_name());
+        existingUser.setLast_name(userDto.getLast_name());
+        existingUser.setEmail(userDto.getEmail());
+        existingUser.setUserGroup(userGroup);
+        existingUser.setStatus(userDto.getStatus());
+        var updatedUser = userService.save(existingUser);
+        return ResponseEntity.ok(new UserDto(updatedUser));
+    }
+
     @PostMapping("v1/login")
     public ResponseEntity<?> login(@RequestBody UserDto userDto) {
         User user = userService.findByUserCodeAndStatus(userDto.getUsercode(), StatusEnum.ACTIVE);
         if (user != null && user.getPassword().equals(userDto.getPassword())) {
             Map<String, Object> response = new HashMap<>();
-            var responseDto = new LoginDto (user);
-            
+            var responseDto = new LoginDto(user);
+
             response.put("userDetails:  ", responseDto);
             response.put("loggedInAt:   ", LocalDateTime.now());
             return ResponseEntity.ok(response);

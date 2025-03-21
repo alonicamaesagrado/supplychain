ee046450d578acd25592d43782fb0ea8b98a08d5
itsalonica <alonicamaesagrado@gmail.com>
2025-03-21 08:37:03 +0800

    fix running balance incorrect output

--- src/main/java/com/informatics/supplychain/service/StockCardService.java
+++ src/main/java/com/informatics/supplychain/service/StockCardService.java
@@ -24,16 +24,16 @@ public class StockCardService {
 
     @Autowired
     StockInRepository stockInRepository;
-    
+
     @Autowired
     StockOutRepository stockOutRepository;
 
     @Autowired
     AssembleDetailRepository assembleDetailRepository;
-    
+
     @Autowired
     AssembleRepository assembleRepository;
-    
+
     @Autowired
     SalesorderDetailRepository salesorderDetailRepository;
 
@@ -41,84 +41,85 @@ public class StockCardService {
         List<StockIn> stockIns = stockInRepository.findByItem_Id(itemId); //rawmats in
         List<AssembleDetail> assembleDetails = assembleDetailRepository.findByRawMaterial_Id(itemId); //rawmats out
         List<StockOut> stockOuts = stockOutRepository.findByItemId(itemId); //rawmats out
-        
+
         List<Assemble> assembles = assembleRepository.findByFinishProduct_Id(itemId); //finishproduct in
         List<SalesorderDetail> salesorderDetails = salesorderDetailRepository.findByItem_Id(itemId); //finishproduct out
 
         List<StockCardDto> stockCardList = new ArrayList<>();
-        double runningBalance = 0;
 
         //rawmats in transactions
         for (StockIn stockIn : stockIns.stream().filter(s -> TransactionStatusEnum.COMPLETED.equals(s.getStatus())).collect(Collectors.toList())) {
-            runningBalance += stockIn.getQuantity();
             stockCardList.add(new StockCardDto(
                     stockIn.getItem().getId(),
                     stockIn.getTransactionDate(),
                     stockIn.getTransactionNo(),
                     stockIn.getQuantity(), //in
                     0.0, //out
-                    runningBalance,
+                    0.0, //running balace
                     stockIn.getCreatedDateTime()
             ));
         }
 
         //rawmats out transactions
         for (AssembleDetail detail : assembleDetails.stream().filter(d -> TransactionStatusEnum.COMPLETED.equals(d.getAssemble().getStatus())).collect(Collectors.toList())) {
-            runningBalance -= detail.getUsedQuantity();
             stockCardList.add(new StockCardDto(
                     detail.getRawMaterial().getId(),
                     detail.getAssemble().getTransactionDate(),
                     detail.getAssemble().getTransactionNo(),
                     0.0, //in
                     detail.getUsedQuantity(), //out
-                    runningBalance,
+                    0.0, //running balace
                     detail.getAssemble().getCreatedDateTime()
             ));
         }
-        
+
         //rawmats out transactions
         for (StockOut stockOut : stockOuts.stream().filter(s -> TransactionStatusEnum.COMPLETED.equals(s.getStatus())).collect(Collectors.toList())) {
-            runningBalance -= stockOut.getQuantity();
             stockCardList.add(new StockCardDto(
                     stockOut.getItem().getId(),
                     stockOut.getTransactionDate(),
                     stockOut.getTransactionNo(),
                     0.0, //in
                     stockOut.getQuantity(), //out
-                    runningBalance,
+                    0.0, //running balace
                     stockOut.getCreatedDateTime()
             ));
         }
-        
+
         //finish product in transactions
         for (Assemble assemble : assembles.stream().filter(d -> TransactionStatusEnum.COMPLETED.equals(d.getStatus())).collect(Collectors.toList())) {
-            runningBalance += assemble.getAssemble_quantity();
             stockCardList.add(new StockCardDto(
                     assemble.getFinishProduct().getId(),
                     assemble.getTransactionDate(),
                     assemble.getTransactionNo(),
                     assemble.getAssemble_quantity(), //in
                     0.0, //out
-                    runningBalance,
+                    0.0, //running balace
                     assemble.getCreatedDateTime()
             ));
         }
-        
+
         //finish product out transactions
         for (SalesorderDetail salesorderdetail : salesorderDetails.stream().filter(d -> TransactionStatusEnum.COMPLETED.equals(d.getSalesorder().getOrderDate())).collect(Collectors.toList())) {
-            runningBalance -= salesorderdetail.getOrderQuantity();
             stockCardList.add(new StockCardDto(
                     salesorderdetail.getId(),
                     salesorderdetail.getSalesorder().getOrderDate(),
                     salesorderdetail.getSalesorder().getSalesorderNo(),
                     0.0, //in
                     salesorderdetail.getOrderQuantity(), //out
-                    runningBalance,
+                    0.0, //running balace
                     salesorderdetail.getSalesorder().getCreatedDateTime()
             ));
         }
-        
+
         stockCardList.sort(Comparator.comparing(StockCardDto::getDate).thenComparing(StockCardDto::getCreatedDateTime));
+        
+        //update running balance based on sorted data
+        double runningBalance = 0;
+        for (StockCardDto stockCard : stockCardList) {
+            runningBalance += stockCard.getStockIn() - stockCard.getStockOut();
+            stockCard.setRunningBalance(runningBalance);
+        }
         return stockCardList;
     }
 
